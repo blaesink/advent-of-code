19eaf1654bb055e97c83cdbc80e82e5f85df2c16
Kevin Blaesing <blaesink@github.com>
2023-06-15 21:07:51 -0400

    day 1 complete

--- rust/src/solutions/y2015/day1.rs
+++ rust/src/solutions/y2015/day1.rs
@@ -1,12 +1,47 @@
-use crate::problem::Solution;
+use crate::problem::{load, Solution};
 
 pub struct Day1;
 
 impl Solution for Day1 {
     fn a(&self) -> String {
-        todo!()
+        let input = load(2015, 1);
+
+        let mut floor = 0;
+
+        input.chars().for_each(|char| match char {
+            '(' => {
+                floor += 1;
+            }
+            ')' => {
+                floor -= 1;
+            }
+            _ => (),
+        });
+
+        floor.to_string()
     }
+    /// Naive solution.
     fn b(&self) -> String {
-        todo!()
+        let input = load(2015, 1);
+        let mut floor = 0;
+        let mut i = 0;
+
+        for (_, char) in input.chars().enumerate() {
+            match char {
+                '(' => {
+                    floor += 1;
+                }
+                ')' => {
+                    floor -= 1;
+                }
+                _ => (),
+            }
+            i += 1;
+            if floor < 0 {
+                break;
+            }
+        }
+
+        i.to_string()
     }
 }
