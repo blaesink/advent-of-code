d7569068a1fb479de0e2f90847ccdffe1a6d70e4
Kevin Blaesing <kevin.blaesing@gmail.com>
2023-12-23 03:32:16 +0000

    python: day 4 part 4
    taking a page from the book of knowing that we're just putting things in buckets

--- 2023/python/src/day4.py
+++ 2023/python/src/day4.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import typer
 import re
 
@@ -32,32 +33,18 @@ def part_one(lines: list[str]) -> int:
 
 
 def part_two(lines: list[str]) -> int:
-    num_scratchcards: int = 0
-
-    # TODO: this should just be the precomputed set of won cards.
-    won_cards: list[str] = []
+    matches = [1] * len(lines)
 
     for idx, line in enumerate(lines):
-        matches = generate_matches_from_line(line)
-        if len(matches) > 0:
-            num_scratchcards += 1
+        num_matches = len(generate_matches_from_line(line))
 
-            if idx + len(matches) > len(lines):
-                won_cards += lines[idx:]
-            else:
-                won_cards += lines[idx : idx + len(matches)]
+        if not num_matches:
+            continue
 
-    for idx, card in enumerate(won_cards):
-        matches = generate_matches_from_line(card)
-        if len(matches) > 0:
-            num_scratchcards += 1
+        for j in range(num_matches):
+            matches[idx + j + 1] += matches[idx]
 
-            if idx + len(matches) > len(lines):
-                won_cards += lines[idx:]
-            else:
-                won_cards += lines[idx : idx + len(matches)]
-
-    return num_scratchcards
+    return sum(matches)
 
 
 def main(data_path: str):
@@ -65,38 +52,35 @@ def main(data_path: str):
         data = [line.strip() for line in f.readlines()]
 
     print("Part one:", part_one(data))
-    # print("Part two:", part_two(data))
+    print("Part two:", part_two(data))
 
 
 if __name__ == "__main__":
     typer.run(main)
 
 
-class Test:
-    @staticmethod
-    def test_part_one():
-        input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-        """
+def test_part_one():
+    input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """
 
-        lines = list(map(str.strip, input.split("\n")))
+    lines = list(map(str.strip, input.split("\n")))
 
-        assert part_one(lines) == 13
+    assert part_one(lines) == 13
 
-    @staticmethod
-    def test_part_two():
-        input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-        """
 
-        lines = list(map(str.strip, input.split("\n")))
+def test_part_two():
+    input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
-        assert part_two(lines) == 30
+    lines = list(map(str.strip, input.split("\n")))
+
+    assert part_two(lines) == 30
